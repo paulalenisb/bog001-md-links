654f37c34ae498fb1967cc8971f1765976130af8
Paula Lenis B <paulalenisb@gmail.com>
2020-10-16 13:27:32 -0500

    resolviendo bugs

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+const { program } = require('commander');
 const mdLinks = require('../src/mdlinks.js');
 const getStatsMDLinks = require('../src/statsValidate.js');
-const { program } = require('commander');
+
 const route = process.argv[2];
 const isValidate = true;
 
@@ -10,36 +12,35 @@ program
   .option('-v, --validate', 'returns all validate links in .md files')
   .option('-s, --stats', 'returns total and unique stats of the links')
   .option('-v -s, --validate --stats', 'returns total, unique, broken and ok')
-  .option('-s -v, --stats --validate', 'Returns total, unique, broken and ok')
+  .option('-s -v, --stats --validate', 'Returns total, unique, broken and ok');
 
 program.parse(process.argv);
 
 // Si solo paso el path
 if (!program.validate && !program.stats) {
-  mdLinks(route, {validate:false}).then(res => console.log(res))
-  .catch((err) => console.error(err))
+  mdLinks(route, { validate: false }).then((res) => console.log(res))
+    .catch((err) => console.error(err));
 }
 
-//Si paso path --validate
+// Si paso path --validate
 if (program.validate && !program.stats) {
-  mdLinks(route, {validate:true}).then(res => console.log(res))
-  .catch((err) => console.error(err))
+  mdLinks(route, { validate: true }).then((res) => console.log(res))
+    .catch((err) => console.error(err));
 }
 
-//Si paso path --stats
+// Si paso path --stats
 if (!program.validate && program.stats) {
-    mdLinks(route, {validate:false}).then(res => {
+  mdLinks(route, { validate: false }).then((res) => {
     const onlyStats = getStatsMDLinks(res);
-    console.log(onlyStats)
+    console.log(onlyStats);
   })
-  .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 }
 
-//Si paso path --stats --validate
-if (!!program.validate && !!program.stats){
-  mdLinks(route, {validate:true}).then(res => {
-    console.log(getStatsMDLinks(res, isValidate))
+// Si paso path --stats --validate
+if (!!program.validate && !!program.stats) {
+  mdLinks(route, { validate: true }).then((res) => {
+    console.log(getStatsMDLinks(res, isValidate));
   })
-  .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 }
-
